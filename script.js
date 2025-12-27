7446da63ba68ccdd84c36ec6812ebf7fde99b8fb
Tore Bjastad <tore.bjastad@gmail.com>
2025-12-27 21:35:42 +0100

    Fail flash animation: Switching to red flash country color instead of border width change.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
   }
 
   // Increment this when you update map files to force reload
-  const APP_VERSION = '3.4';
+  const APP_VERSION = '3.5';
   const DEBUG_TOUCH = false;
 
   const mapCache = new Map();
